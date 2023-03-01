a91baa95afd1fc5fe3d6d0015a87dce649f46376
Nikita Justin Olechko <96552468+nikita-olechko@users.noreply.github.com>
2023-02-28 22:47:02 -0800

    Add files via upload

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,9 +1,15 @@
+
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
     //keys go here 
-  
+    apiKey: "AIzaSyCEJEiKKNqD6dFLdgOWhOXv8N3_dB3zL0s",
+    authDomain: "comp1800-ba088.firebaseapp.com",
+    projectId: "comp1800-ba088",
+    storageBucket: "comp1800-ba088.appspot.com",
+    messagingSenderId: "496254016562",
+    appId: "1:496254016562:web:156aa56b7cc16cd6f2b02b"
 };
 
 //--------------------------------------------
